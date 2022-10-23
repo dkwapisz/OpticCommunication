9411f462f6aa1372ff7562e7d66d09e7152eebec
Wiktor Tekiela <56120539+abcdrefg@users.noreply.github.com>
2022-10-23 22:11:15 +0200

    RC5 implementation decoder

---
+++ app/src/main/java/com/example/opticcommunication/decoder/DecoderRC5.java
@@ -0,0 +1,27 @@
+package com.example.opticcommunication.decoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.StandardCharsets;
+
+public class DecoderRC5 {
+
+    private static final int frameLength = 12;
+    private DecoderRC5() {}
+
+    public static String decodeBitBuffer(String bitBuffer) {
+        String[] rc5Frames = bitBuffer.split("(?<=\\G.{" + (DecoderRC5.frameLength - 1) + "})");
+        StringBuilder message = new StringBuilder();
+        for (String frame : rc5Frames) {
+            message.append(decodeFrame(frame));
+        }
+        return message.toString();
+    }
+
+    private static String decodeFrame(String frame) {
+        String byt = frame.substring(3);
+        byte characterToDecode = Byte.parseByte(frame.substring(3), 2);
+        return new String(new byte[]{characterToDecode}, StandardCharsets.UTF_8);
+    }
+}
